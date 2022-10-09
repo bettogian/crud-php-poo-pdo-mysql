a3a04a62dcd5942a7e1ec8df4fbb4126fb4ecdaf
Humberto Romero <hipopo@gmail.com>
2022-10-09 00:12:50 -0500

    Update config.php

--- env/config.php
+++ env/config.php
@@ -2,9 +2,9 @@
 	
 	define('DB_DRIVER', "mysql");
 		
-	define('DB_HOST', "localhost");
-	define('DB_USER', "root");
-	define('DB_PASSWORD', "");
+	define('DB_HOST', "####");
+	define('DB_USER', "####");
+	define('DB_PASSWORD', "####");
 	define('DB_NAME', "movies_db");
 
 	define('DB_CHARSET', "utf8mb4");
