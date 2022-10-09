31f622cdba3634a4361e205a2a0d53ca65628403
Humberto Romero <hipopo@gmail.com>
2022-10-09 01:10:14 -0500

    Add files via upload

---
+++ env/config.php
@@ -0,0 +1,13 @@
+<?php
+	
+	define('DB_DRIVER', "mysql");
+		
+	define('DB_HOST', "####");
+	define('DB_USER', "####");
+	define('DB_PASSWORD', "####");
+	define('DB_NAME', "####");
+
+	define('DB_CHARSET', "utf8mb4");
+	
+	
+?>
